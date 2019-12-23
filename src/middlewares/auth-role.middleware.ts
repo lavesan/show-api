a14c4b95cb5aca2fdbb752747914ee6a2f98e348
valdery <valdery.jur@gmail.com>
2019-12-23 01:38:24 -0300

    :Adiciona middleware para verificar se o usuário backoffice é admin:
    Signed-off-by: valdery <valdery.jur@gmail.com>

---
+++ src/middlewares/auth-role.middleware.ts
@@ -0,0 +1,29 @@
+
+import { Injectable, NestMiddleware, HttpException, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as jwtDecode from 'jwt-decode';
+
+import { UserBackofficeRole } from 'src/model/constants/userBackoffice.constants';
+
+@Injectable()
+export class AdminMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    if (req.headers.authorization) {
+        // Assim eu verifico se o usuário tem a role necessária
+        const { role } = jwtDecode(req.headers.authorization);
+
+        if(role === UserBackofficeRole.ADMIN) {
+            next();
+        } else {
+            throw new HttpException({
+                status: HttpStatus.FORBIDDEN,
+                error: 'Tipo de usuário inválido',
+            }, HttpStatus.FORBIDDEN);
+        }
+    }
+    throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'Token de autorização inválido',
+    }, HttpStatus.FORBIDDEN);
+  }
+}
