7aebaaaeeeaf75abb2211368ccbc8b21ccffb1cd
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2019-12-25 18:07:21 -0300

    :sparkles: Adiciona bibliotecas para proteger o uso por cliente
    - 'helmet' para setar os headers da request corretamente
    - 'express-rate-limit' para limitar a quantidade de request de usuários por segundo
    - 'csurf' para bloquear entradas maliciosas
    
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: {
@@ -8,6 +10,12 @@ async function bootstrap() {
     methods: ['PUT', 'POST', 'GET', 'DELETE']
   } });
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.use(rateLimit({
+    windowMs: 1000, // 1 segundo window
+    max: 10, // start blocking after 5 requests
+    message: 'Muitas requisições estão sendo feitas por este IP. Espere 15 minutos para voltar a efetuar',
+  }));
   await app.listen(3000);
 }
 bootstrap();
