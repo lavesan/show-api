2f23f541ee8536188da55573f758eb55286443c7
valdery-mangue3 <valderyalves@mangue3.com>
2020-02-03 17:01:43 -0300

    :construction: Utiliza 2 serviços com 2 módulos e 2 secret-keys diferentes para serem autenticadas por JWT
    Signed-off-by: valdery-mangue3 <valderyalves@mangue3.com>

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,22 +5,16 @@ import { AuthController } from '../../controllers/client/auth/auth.controller';
 import { AuthService } from '../../services/auth/auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtStrategy } from 'src/services/auth/jwt.strategy';
-import { AdminAuthService } from 'src/services/admin-auth/admin-auth.service';
-import { UserBackofficeModule } from '../user-backoffice/user-backoffice.module';
-import { JwtAdminStrategy } from 'src/services/admin-auth/jwt-admin.strategy';
 
 @Module({
     controllers: [AuthController],
     imports: [
         UserModule,
-        UserBackofficeModule,
         PassportModule,
     ],
     providers: [
         AuthService,
-        AdminAuthService,
         JwtStrategy,
-        JwtAdminStrategy,
     ],
 })
 export class AuthModule {}
