90e5f1dfda95bbae38eac197c13ab586e33ebd1b
valdery-mangue3 <valderyalves@mangue3.com>
2020-02-04 12:48:49 -0300

    :sparkles: Adiciona formulário de confiurmação de email para o usuário cliente
    Signed-off-by: valdery-mangue3 <valderyalves@mangue3.com>

--- src/model/constants/sendgrid.constants.ts
+++ src/model/constants/sendgrid.constants.ts
@@ -1,6 +1,5 @@
 export enum MailType {
     FORGOT_PWD_ADMIN = 'forgotPasswordAdmin',
     FORGOT_PWD_CLIENT = 'forgotPasswordClient',
-    CONFIRM_MAIL_ADMIN = 'confirmAdmin',
     CONFIRM_MAIL_CLIENT = 'confirmClient',
 }
