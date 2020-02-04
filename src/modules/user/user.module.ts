61b674c1c4fc86302d3321e86a6042edac74600d
valdery-mangue3 <valderyalves@mangue3.com>
2020-02-04 12:49:45 -0300

    :scissors: Remove módulo do sendgrid do módulo de usuário cliente e backoffice
    Signed-off-by: valdery-mangue3 <valderyalves@mangue3.com>

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,13 +5,11 @@ import { UserService } from '../../services/user/user.service';
 import { UserEntity } from '../../entities/user.entity';
 import { BackofficeUserController } from 'src/controllers/backoffice/user/backoffice-user.controller';
 import { UserController } from 'src/controllers/client/user/user.controller';
-import { SendgridModule } from '../sendgrid/sendgrid.module';
 
 @Module({
     imports: [
       TypeOrmModule.forFeature([UserEntity]),
       UserModule,
-      SendgridModule,
     ],
     controllers: [UserController, BackofficeUserController],
     providers: [UserService],
