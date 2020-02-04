fbea74d1a14a7895d817e01bf23b75b3c8b3ad49
valdery-mangue3 <valderyalves@mangue3.com>
2020-02-04 11:58:49 -0300

    :construction: Adiciona os tipos de email usados no SendGridService em um enum
    Signed-off-by: valdery-mangue3 <valderyalves@mangue3.com>

---
+++ src/model/constants/sendgrid.constants.ts
@@ -0,0 +1,6 @@
+export enum MailType {
+    FORGOT_PWD_ADMIN = 'forgotPasswordAdmin',
+    FORGOT_PWD_CLIENT = 'forgotPasswordClient',
+    CONFIRM_MAIL_ADMIN = 'confirmAdmin',
+    CONFIRM_MAIL_CLIENT = 'confirmClient',
+}
