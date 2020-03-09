a3a5913eb77dd8fa9f2ccd85a802cfb99c53fb29
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-03-09 14:32:49 -0300

    :bug: Remove o módulo de usuário do módulo de contato e endereço e adiciona os mesmos no módulo de usuário
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -3,13 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressService } from '../../services/address/address.service';
 import { AddressEntity } from '../../entities/address.entity';
 import { AddressController } from 'src/controllers/address/address.controller';
-import { UserModule } from '../user/user.module';
 
 @Module({
     imports: [
       TypeOrmModule.forFeature([AddressEntity]),
       AddressModule,
-      UserModule,
     ],
     controllers: [AddressController],
     providers: [AddressService],
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,10 +5,14 @@ import { UserService } from '../../services/user/user.service';
 import { UserEntity } from '../../entities/user.entity';
 import { BackofficeUserController } from 'src/controllers/backoffice/user/backoffice-user.controller';
 import { UserController } from 'src/controllers/client/user/user.controller';
+import { ContactModule } from '../contact/contact.module';
+import { AddressModule } from '../address/address.module';
 
 @Module({
     imports: [
       TypeOrmModule.forFeature([UserEntity]),
+      ContactModule,
+      AddressModule,
       UserModule,
     ],
     controllers: [UserController, BackofficeUserController],
